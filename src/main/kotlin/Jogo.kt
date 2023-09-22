e71c07108f3c2ca459d21285f8d5ebbdc10bfa1e
marciliodevjava <marciliodevjava@gmail.com>
2023-09-21 21:43:01 -0300

    Crete Jogo

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,14 @@
+import com.ibm.jvm.trace.format.api.Message.Arg
+
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Titulo: $titulo, \n" +
+                "Capa: $capa, \n" +
+                "Descricao: $descricao\n"
+    }
+}
