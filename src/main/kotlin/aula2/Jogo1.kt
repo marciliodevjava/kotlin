23fe58c2802f4e29d0be69ab98676a10e5f35568
marciliodevjava <marciliodevjava@gmail.com>
2023-09-21 21:56:30 -0300

    Create Jogo1

---
+++ src/main/kotlin/aula2/Jogo1.kt
@@ -0,0 +1,12 @@
+package aula2
+
+class Jogo1(val titulo:String, val capa:String) {
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Titulo: $titulo, \n" +
+                "Capa: $capa, \n" +
+                "Descricao: $descricao\n"
+    }
+}
