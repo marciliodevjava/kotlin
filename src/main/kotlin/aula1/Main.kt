629d74f7ee730d214da704a89fcacc7221147ebd
marciliodevjava <marciliodevjava@gmail.com>
2023-09-22 18:01:55 -0300

    Update Main1

--- src/main/kotlin/aula1/Main.kt
+++ src/main/kotlin/aula1/Main.kt
@@ -1,13 +1,12 @@
 package aula1
 
-import aula1.Jogo
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
 
 
-fun main(){
+fun main() {
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
         .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
