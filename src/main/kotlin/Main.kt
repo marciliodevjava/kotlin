f3dc879725d58d599e3fdbe0cdf183de19bab1a6
marciliodevjava <marciliodevjava@gmail.com>
2023-09-21 21:43:11 -0300

    Update Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
 
-fun main() {
+fun main(){
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
 
+    val response = client.send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+
+    println(json)
+
+    val meuJogo = Jogo()
+    meuJogo.titulo = "Batman: Guerreiro Negro"
+    meuJogo.capa = "https://pt.wikipedia.org/wiki/Batman_%281989%29#/media/Ficheiro:Batman_ver2.jpg"
+
+    println(meuJogo)
 }
